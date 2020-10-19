faafc2ac17e17afb2358b3bb589ca054a295eb2d
ODEUFPB <66695623+ODEUFPB@users.noreply.github.com>
2020-10-18 23:06:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -42,9 +42,7 @@ from logging import FileHandler, WARNING
 
 
 app =  dash.Dash(__name__, external_stylesheets=[dbc.themes.UNITED],     
-        meta_tags=[
-        {"name": "viewport", "content": "width=device-width, initial-scale=1"}         
-    ])
+)
 server = app.server
       
 app.config.suppress_callback_exceptions = True   
